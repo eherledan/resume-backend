c48c28da69ca0a290b6d981f044c55dd53ef03b4
Falathar <falathar@outlook.fr>
2018-09-27 00:17:34 +0200

    add headers and fix response

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ app.listen(8000, () => {
     console.log('Server started!');
 });
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.post('/api/email', function (req, res) {
     let options, transport;
     transport = nodemailer.createTransport({
@@ -32,10 +51,10 @@ app.post('/api/email', function (req, res) {
 
     transport.sendMail(options, function (error, response) {
         if (error) {
-            res.send(error);
+            res.send({response: 'ERROR'});
         }
         else {
-            res.send('SUCCESS');
+            res.send({response: 'SUCCESS'});
         }
     });
 });
